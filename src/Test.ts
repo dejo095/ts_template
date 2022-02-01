69816ab0d88048c7dfe0c5bcaf25653c7eb8d1ac
dejo095 <dejo095@gmail.com>
2022-02-01 22:30:48 +0100

    initial commit

---
+++ src/Test.ts
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+export default class Test {
+
+    async testMethod() {
+        return axios.get('');
+    }
+
+}
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import Test from './Test';
+
+const test = new Test();
+
+test.testMethod();
